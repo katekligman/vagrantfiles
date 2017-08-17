3582a0ed429503394b5a98e945d7e78697ec136a
Kate Kligman <kate@katekligman.com>
2017-08-17 14:59:04 -0700

    Add profile customizations

---
+++ ubuntu64-zesty/files/profile-customizations.sh
@@ -0,0 +1,6 @@
+alias emacs='emacs --color=no'
+alias history='\history'
+alias ls='\ls -pc1'
+alias grep='grep --color=never'
+
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
--- ubuntu64-zesty/provision.sh
+++ ubuntu64-zesty/provision.sh
@@ -7,8 +7,10 @@ apt-get install -y python3 python3-pip npm nodejs-legacy git
 pip3 install virtualenvwrapper django djangorestframework markdown django-filter
 npm install -g grunt-cli bower
 
-echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> /home/vagrant/.profile
-
 # Install sound
 apt-get install -y alsa alsa-utils
 apt-get install -y linux-image-extra-`uname -r`
+
+# Copy profile settings
+cp files/profile-customizations.sh /etc/profile.d
+
